a8981640784f3ee90d80f223c2413f100ea9968e
NuGG37SZ <144650779+NuGG37SZ@users.noreply.github.com>
2025-02-27 09:45:16 +0400

    commit

--- auth.js
+++ auth.js
@@ -2,6 +2,7 @@ const redirectSignFormBtn = document.getElementById('redirect-sign');
 const redirectRegFormBtn = document.getElementById('redirect-reg');
 const authSection = document.querySelector('.auth-section');
 const regSection = document.querySelector('.registration-section');
+const authBtn = document.getElementById('auth-btn');
 
 redirectSignFormBtn.addEventListener('click', () => {
     swapDisplay(regSection, authSection);
@@ -14,4 +15,18 @@ redirectRegFormBtn.addEventListener('click', () => {
 function swapDisplay(first, second) {
     first.style.display = 'none';
     second.style.display = 'block';
+}
+
+authBtn.addEventListener('click', () => {
+    const loginInput = document.getElementById('loginInputAuth');
+    const passwordInput = document.getElementById('passwordInputAuth');
+
+    requestGetUserByLogin(loginInput.textContent);
+})
+
+
+function requestGetUserByLogin(login) {
+    fetch(`http://localhost:5098/users/getByLogin/${login}`)
+        .then(response => response.json())
+        .then(user => console.log(user))
 }
