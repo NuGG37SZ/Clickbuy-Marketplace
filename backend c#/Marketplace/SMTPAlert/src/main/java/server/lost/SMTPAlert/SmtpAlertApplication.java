886606fd5eb543093d4832256bcdc763e1c527d5
Lostmore <secondmyworld@gmail.com>
2025-03-15 14:29:39 +0300

    Minor

---
+++ backend c#/Marketplace/SMTPAlert/src/main/java/server/lost/SMTPAlert/SmtpAlertApplication.java
@@ -0,0 +1,13 @@
+package server.lost.SMTPAlert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmtpAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmtpAlertApplication.class, args);
+	}
+
+}
--- backend c#/Marketplace/SMTPAlert/src/main/java/server/lost/SMTPAlert/controllers/UserController.java
+++ backend c#/Marketplace/SMTPAlert/src/main/java/server/lost/SMTPAlert/controllers/UserController.java
@@ -1,2 +1,22 @@
-package server.lost.SMTPAlert.controllers;public class UserController {
+package server.lost.SMTPAlert.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+import server.lost.SMTPAlert.UserServices.UserClientService;
+import server.lost.SMTPAlert.UserServices.UserDTO;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserClientService userClientService;
+
+    public UserController(UserClientService userClientService) {
+        this.userClientService = userClientService;
+    }
+
+    @GetMapping("/getByLogin/{login}")
+    public Mono<UserDTO> getUserByLogin(@PathVariable String login) {
+        return userClientService.getUserByLogin(login);
+    }
 }
